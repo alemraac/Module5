00a8c549deb816e762ccc70627304d297aeba1b1
FIRST_NAME LAST_NAME <johnhenry.mariano.iics@ust.edu.ph>
2020-10-02 02:18:36 +0800

    Added app.js and modified module5.html

---
+++ app.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+        fs.readFile('html/module5.html', function(err, data){
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    } else if(req.url === "/mariano" || req.url === "/mariano.html"){
+        fs.readFile('html/supan.html', function(err, data) {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.write("<a href='/'><center>Back to Home Page</center></a>");
+            res.end();
+        })
+    } else if(req.url === "/laurel" || req.url === "/laurel.html"){
+        fs.readFile('html/delapena.html', function(err, data) {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.write("<a href='/'><center>Back to Home Page</center></a>");
+            res.end();
+        })
+    } else if(req.url === "/laroco" || req.url === "/laroco.html"){
+        fs.readFile('html/bozar.html', function(err, data) {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.write("<a href='/'><center>Back to Home Page</center></a>");
+            res.end();
+        })
+    }else {
+        res.writeHead(404, {"Content-Type":"text/html"});
+        res.write("<html><body><h1>404: Page Not Found</h1></body></html>");
+        res.write("<a href='/'><center>Back to Home Page</center></a>");
+        res.end();
+    }
+}).listen(4000);
+console.log("listening to port 4000");
